f9b87d78c77b965f30f41965354825463994edd8
yuhexiong <yuhexiong871210@gmail.com>
2023-12-02 19:33:36 +0800

    feat: PPT Gossiping Crawling

---
+++ README.md
@@ -0,0 +1,10 @@
+# PPT Gossiping Crawling
+
+set pages you want to crawl in line 5
+mkdir data before run
+
+
+## Version
+
+- Python v3.9.15
+- requests v2.28.1
---
+++ pptGossipingCrawl.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup as bs
+import requests as req
+import json
+
+for page in range(1, 30001):
+    url = f"https://www.ptt.cc/bbs/Gossiping/index{page}.html" 
+    over18 = {
+        "over18": "1"
+    }
+    res = req.get(url, cookies = over18) 
+
+    soup = bs(res.text, "lxml") 
+
+    link = []
+    for l in soup.select('div.r-ent > div.title > a'):
+        link.append('https://www.ptt.cc'+l['href']) 
+
+    articles = []
+    for subLink in link:
+        subRes = req.get(subLink, cookies = over18)
+        subSoup = bs(subRes.text, "lxml")
+        article = subSoup.select_one("#main-content").get_text()
+        content = article.split("※ 發信站:")[0]
+        articles.append(content.replace("\n",""))
+
+    with open(f"data/ppt{page}.json", "w", encoding="utf-8") as file:
+        file.write( json.dumps(articles, ensure_ascii=False) )
